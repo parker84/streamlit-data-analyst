fd3dcc3a45ed012a438fc5706468db8a0367dbd6
parker84 <brydonparker4@gmail.com>
2023-08-19 15:28:06 -0400

    writing the code written back into the output and wiping anything written during the analysis process

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
 from langchain.agents.agent_types import AgentType
 from dotenv import find_dotenv, load_dotenv
+import streamlit as st
 import pandas as pd
 load_dotenv(find_dotenv())
 
@@ -13,17 +14,27 @@ pd_agent = create_csv_agent(
     agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
 )
 
-result = pd_agent.run(
-    f"""
-    Create me an entire functioning streamlit dashboard to analyze this data.
-    Plot trends over time, use st.metrics to display revelant big numbers.
-    Use bar charts to show breakdowns by different categories.
-    
-    Return the python script I can use to create the entire dashboard.
-    The filepath to the csv you're analyzing is is {csv}
-    """
-)
+placeholder = st.empty()
+with placeholder:
+    result = pd_agent.run(
+        f"""
+        Create me an entire functioning streamlit dashboard to analyze this data.
+        Plot trends over time, use st.metrics to display revelant big numbers.
+        Use bar charts to show breakdowns by different categories.
+        
+        Return the python script I can use to create the entire dashboard.
+        The filepath to the csv you're analyzing is is {csv}
+        """
+    )
+placeholder.empty()
 
-import ipdb; ipdb.set_trace()
 exec(result)
+with st.expander('Code Written by Agent'):
+    st.markdown(
+f"""
+```py
+{result}
+```
+"""
+)
 # result = pd_agent.run("how many rows are in this dataset?")
