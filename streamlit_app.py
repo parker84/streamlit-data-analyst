ea13479849ca2f1e8d6472acbf846de65196847c
parker84 <brydonparker4@gmail.com>
2023-08-19 15:43:42 -0400

    adding caching and some titles

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,12 @@ from langchain.llms import OpenAI
 from langchain.agents.agent_types import AgentType
 from dotenv import find_dotenv, load_dotenv
 import streamlit as st
-import pandas as pd
 load_dotenv(find_dotenv())
+
+st.set_page_config(page_icon='💻')
+st.title('Streamlit Data Analyst 💻')
+
+
 
 csv = "titanic.csv"
 pd_agent = create_csv_agent(
@@ -14,22 +18,28 @@ pd_agent = create_csv_agent(
     agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
 )
 
-with st.spinner('Thinking...'):
-    placeholder = st.empty()
-    with placeholder:
-        result = pd_agent.run(
-            f"""
-            Create me an entire functioning streamlit dashboard to analyze this data.
-            Plot trends over time, use st.metrics to display revelant big numbers.
-            Use bar charts to show breakdowns by different categories.
-            
-            Return the python script I can use to create the entire dashboard.
-            The filepath to the csv you're analyzing is is {csv}
-            """
-        )
-    placeholder.empty()
+@st.cache_data
+def ask_the_csv_agent(prompt):
+    result = pd_agent.run(prompt)
+    return result
+
+placeholder = st.empty()
+with placeholder:
+    result = ask_the_csv_agent(
+        f"""
+        Create me an entire functioning streamlit dashboard to analyze this data.
+        Plot trends over time, use st.metrics to display revelant big numbers.
+        Use bar charts to show breakdowns by different categories.
+        
+        Return the python script I can use to create the entire dashboard.
+        The filepath to the csv you're analyzing is is {csv}
+        """
+    )
+placeholder.empty()
 
 exec(result)
+
+st.markdown('### The Code')
 with st.expander('Code Written by Agent'):
     st.markdown(
 f"""
@@ -38,4 +48,11 @@ f"""
 ```
 """
 )
-# result = pd_agent.run("how many rows are in this dataset?")
+
+st.markdown('### Ask a Specific Question')
+question = st.text_input(
+    label='Ask a Specific Question About the Dataset',
+    value='How many people are over the age of 65?'
+)
+result = ask_the_csv_agent(f"{question} (display response for markdown)")
+st.markdown(result)
